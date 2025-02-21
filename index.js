e83072e22adaef502f0c6a8febf1196f4b559689
pablofm <pablofm@us.es>
2025-02-21 12:28:44 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cool = require("cool-ascii-faces");
 const app = express();
-const PORT = 16078;
+const PORT = process.env.PORT || 16078;
 
 app.use("/",express.static("./public"));
 
@@ -16,7 +16,3 @@ app.get("/cool",(request,response)=>{
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}!`);
 });
-
-console.log(`Finish setup`);
-
-
