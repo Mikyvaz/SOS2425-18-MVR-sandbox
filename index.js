f2e38d1eb0c8f9d0ab71e47cde0917e9aaafeb9c
pablofm <pablofm@us.es>
2025-02-21 12:02:21 +0100

    update

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log("Init script");
+const express = require("express");
+const app = express();
+const PORT = 16078;
+
+app.get("/",(request,response)=>{
+    response.send("Hello from the server!");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}!`);
+});
+
+console.log(`Finish setup`);
+
 
