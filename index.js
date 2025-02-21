e3bcf1125186b707a8d85ff1dee8c492413e2aeb
pablofm <pablofm@us.es>
2025-02-21 12:14:10 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cool = require("cool-ascii-faces");
 const app = express();
 const PORT = 16078;
 
 app.get("/",(request,response)=>{
     response.send("Hello from the server!");
+});
+
+app.get("/cool",(request,response)=>{
+    response.send(cool());
 });
 
 app.listen(PORT,()=>{
