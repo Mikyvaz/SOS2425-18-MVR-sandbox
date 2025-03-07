76f20098daa7d40cacbedda491e58df6f908190d
pablofm <pablofm@us.es>
2025-03-07 12:37:23 +0100

    final code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 16078;
 const BASE_API = "/api/v1";
 
 app.use("/",express.static("./public"));
+app.use(express.json());
 
 let contacts = [
     {
@@ -18,8 +19,23 @@ let contacts = [
 
 app.get(BASE_API+"/contacts",(request,response)=>{
     console.log("New GET to /contacts");
-    response.send(JSON.stringify(contacts));
+    response.send(JSON.stringify(contacts,null,2));
 });
+
+
+app.post(BASE_API+"/contacts",(request,response)=>{
+
+    console.log("POST to /contacts");
+    console.log(`<${request.body}>`);
+
+    let newContact = request.body;
+    
+    contacts.push(newContact);
+
+    response.sendStatus(201);
+});
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}!`);
