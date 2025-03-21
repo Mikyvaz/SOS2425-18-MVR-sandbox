abc8f7976119220f076e4d7ce47c1e330f8e683c
pablofm <pablofm@us.es>
2025-03-21 11:08:59 +0100

    updated code L06

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-const express = require("express");
+//const express = require("express");
+import express from "express";
+import  { loadBackend } from "./src/back/index.js";
+
 const app = express();
 const PORT = process.env.PORT || 16078;
-const BASE_API = "/api/v1";
 
-app.use("/",express.static("./public"));
 app.use(express.json());
+app.use("/",express.static("./public"));
 
-let contacts = [
-    {
-        name: "peter",
-        phone: 123456
-    },
-    {
-        name: "pablo",
-        phone: 789654
-    }
-];
-
-app.get(BASE_API+"/contacts",(request,response)=>{
-    console.log("New GET to /contacts");
-    response.send(JSON.stringify(contacts,null,2));
-});
-
-
-app.post(BASE_API+"/contacts",(request,response)=>{
-
-    console.log("POST to /contacts");
-    console.log(`<${request.body}>`);
-
-    let newContact = request.body;
-    
-    contacts.push(newContact);
-
-    response.sendStatus(201);
-});
-
-
+loadBackend(app)
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}!`);
