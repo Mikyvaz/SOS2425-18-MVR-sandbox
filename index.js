1fc9a7cdf6ae3354d66182035bd8ad089bab865b
pablofm <pablofm@us.es>
2025-03-28 12:27:08 +0100

    l07

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 16078;
 
 app.use(express.json());
 // app.use("/",express.static("./public"));
+
+loadBackend(app);
+
 app.use(handler);
 
-loadBackend(app)
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}!`);
