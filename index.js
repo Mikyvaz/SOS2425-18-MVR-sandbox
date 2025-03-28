ec1dcd4f499f0ebd1b6345de49abae795d57c3fd
pablofm <pablofm@us.es>
2025-03-28 12:15:16 +0100

    l07

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 //const express = require("express");
 import express from "express";
 import  { loadBackend } from "./src/back/index.js";
-
+import { handler} from './src/front/build/handler.js';
+ 
 const app = express();
 const PORT = process.env.PORT || 16078;
 
 app.use(express.json());
-app.use("/",express.static("./public"));
+// app.use("/",express.static("./public"));
+app.use(handler);
 
 loadBackend(app)
 
